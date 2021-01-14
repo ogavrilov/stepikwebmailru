9d981a099ae9dd9c55131c9591cc0ad1fecfef21
ogavrilov <oleg.centre@gmail.com>
2021-01-15 02:07:37 +0300

    add make dir for logs before start nginx

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo mkdir /home/box/web/log
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
