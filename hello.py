c587fe33cc75b0c51bafbd407cf623edda206b04
ogavrilov <oleg.centre@gmail.com>
2021-01-15 23:10:53 +0300

    5td try to repair str to bytes

--- hello.py
+++ hello.py
@@ -2,8 +2,9 @@ def app(env, start_response):
     query_string = env.get("QUERY_STRING")
     data = list()
     if query_string:
-        new_data = str(query_string).replace("&", "\n")
-        data.append(bytes(new_data))
+        new_datas = str(query_string).split("&")
+        for new_data in new_datas:
+            data.append(bytes(new_data))
     #
     headers = list()
     headers.append(("Content-Type", "text/plain"))
