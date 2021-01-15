3ecc5bcd0d3d02628d625f901688aaf5e648aa4c
ogavrilov <oleg.centre@gmail.com>
2021-01-15 17:04:09 +0300

    change code for get byte string in app answer

--- hello.py
+++ hello.py
@@ -2,9 +2,9 @@ def app(env, start_response):
     query_string = env.get("QUERY_STRING")
     data = list()
     if query_string:
-        data.append(str(query_string).replace("&", "\n"))
+        data.append(b'' + str(query_string).replace("&", "\n"))
     #
     headers = list()
     headers.append(("Content-Type", "text/plain"))
     start_response("200 OK", headers)
-    return iter(data)
+    return iter([data])
