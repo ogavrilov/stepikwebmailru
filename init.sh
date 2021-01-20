fab4eb000f739087455761e6f4ed1539ef8495f7
ogavrilov <oleg.centre@gmail.com>
2021-01-21 02:20:37 +0300

    task 2.3.10, 13d try

--- init.sh
+++ init.sh
@@ -4,13 +4,26 @@ sudo apt-get install -y python3.5
 sudo apt-get install -y python3.5-dev
 #
 sudo apt-get install -y debconf-utils
+
+ROOT_PASSWORD=""
+
+echo "mysql-apt-config mysql-apt-config/unsupported-platform select abort" | /usr/bin/debconf-set-selections
+echo "mysql-apt-config mysql-apt-config/repo-codename   select trusty" | /usr/bin/debconf-set-selections
+echo "mysql-apt-config mysql-apt-config/select-tools select" | /usr/bin/debconf-set-selections
+echo "mysql-apt-config mysql-apt-config/repo-distro select ubuntu" | /usr/bin/debconf-set-selections
+echo "mysql-apt-config mysql-apt-config/select-server select mysql-5.7" | /usr/bin/debconf-set-selections
+echo "mysql-apt-config mysql-apt-config/select-product select Apply" | /usr/bin/debconf-set-selections
+
+echo "mysql-community-server mysql-community-server/root-pass password $ROOT_PASSWORD" | /usr/bin/debconf-set-selections
+echo "mysql-community-server mysql-community-server/re-root-pass password $ROOT_PASSWORD" | /usr/bin/debconf-set-selections
+echo "mysql-community-server mysql-community-server/remove-data-dir boolean false" | /usr/bin/debconf-set-selections
+echo "mysql-community-server mysql-community-server/data-dir note" | /usr/bin/debconf-set-selections
+
 export DEBIAN_FRONTEND=noninteractive
-debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-product select Ok'
-sudo wget http://dev.mysql.com/get/mysql-apt-config_0.7.2-1_all.deb
-sudo dpkg -i mysql-apt-config_0.7.2-1_all.deb
+sudo wget http://dev.mysql.com/get/mysql-apt-config_0.6.0-1_all.deb
+sudo dpkg --install mysql-apt-config_0.6.0-1_all.deb
 sudo apt-get update
-sudo apt-get install -y mysql-server-5.7
-sudo apt-get install --assume-yes mysql-server-5.7 mysqlclient
+sudo apt-get --yes --force-yes install mysql-server-5.7
 # prepare virtual environment
 virtualenv -p python3.5 myvenv
 source myvenv/bin/activate
