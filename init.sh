01065030d8787d6360de0f0fe6eb413d13d0d223
ogavrilov <oleg.centre@gmail.com>
2021-01-21 02:52:01 +0300

    task 2.3.10, 17d try

--- init.sh
+++ init.sh
@@ -3,11 +3,22 @@ sudo apt-get update
 sudo apt-get install -y python3.5
 sudo apt-get install -y python3.5-dev
 #
-sudo wget http://repo.mysql.com/mysql-apt-config_0.2.1-1debian7_all.deb
-sudo dpkg -i mysql-apt-config_0.2.1-1debian7_all.deb
-sudo deb http://repo.mysql.com/apt/debian/ wheezy mysql-5.6
+#sudo wget http://repo.mysql.com/mysql-apt-config_0.2.1-1debian7_all.deb
+#sudo dpkg -i mysql-apt-config_0.2.1-1debian7_all.deb
+#sudo deb http://repo.mysql.com/apt/debian/ wheezy mysql-5.6
+#sudo apt-get update
+#sudo apt-get install -y mysql-server-5.6
+sudo apt-get install -y debconf-utils
+export DEBIAN_FRONTEND=noninteractive
+ROOT_PASSWORD=""
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/enable-repo select mysql-5.6'
+debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password password $ROOT_PASSWORD'
+debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password_again password $ROOT_PASSWORD'
+export DEBIAN_FRONTEND=noninteractive
+sudo wget http://dev.mysql.com/get/mysql-apt-config_0.6.0-1_all.deb
+sudo dpkg --install mysql-apt-config_0.6.0-1_all.deb
 sudo apt-get update
-sudo apt-get install mysql-server-5.6
+sudo apt-get --yes --force-yes install mysql-server-5.6
 # prepare virtual environment
 virtualenv -p python3.5 myvenv
 source myvenv/bin/activate
