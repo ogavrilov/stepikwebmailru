12624ab4f24f0efe995c4133fd6d564dcc86ab19
ogavrilov <oleg.centre@gmail.com>
2021-01-21 01:25:46 +0300

    task 2.3.10, 4d try

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.db.backends.mysql.base import DatabaseWrapper
+
+DatabaseWrapper.data_types['DateTimeField'] = 'datetime'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
